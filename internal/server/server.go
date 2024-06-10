93077ebf9428e05ca7d7994e54ee4ee0acfd7e97
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-10 18:09:05 +0800

    Init project

---
+++ internal/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"os"
+
+	"github.com/curtisnewbie/acct/internal/schema"
+	"github.com/curtisnewbie/miso/miso"
+)
+
+func init() {
+	miso.PreServerBootstrap(func(rail miso.Rail) error {
+		rail.Infof("acct version: %v", Version)
+		return nil
+	})
+}
+
+func BootstrapServer() {
+	// automatic MySQL schema migration using svc
+	schema.EnableSchemaMigrate()
+	miso.PreServerBootstrap(PreServerBootstrap)
+	miso.PostServerBootstrapped(PostServerBootstrap)
+	miso.BootstrapServer(os.Args)
+}
+
+func PreServerBootstrap(rail miso.Rail) error {
+	// declare http endpoints, jobs/tasks, and other components here
+	return nil
+}
+
+func PostServerBootstrap(rail miso.Rail) error {
+	// do stuff right after server being fully bootstrapped
+	return nil
+}
