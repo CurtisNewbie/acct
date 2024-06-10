c55c2c6176a791f8fb5f27319687bd39f4c39c1d
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-10 21:19:55 +0800

    cashflow schema

--- internal/schema/scripts/schema.sql
+++ internal/schema/scripts/schema.sql
@@ -1,1 +1,28 @@
 -- Initialize schema
+
+create database acct;
+
+use acct;
+
+CREATE TABLE `cashflow` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `user_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'user no',
+  `direction` varchar(6) NOT NULL DEFAULT '' COMMENT 'flow direction: IN / OUT',
+  `trans_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'transaction time',
+  `trans_id` varchar(64) NOT NULL DEFAULT '' COMMENT 'transaction id',
+  `counterparty` varchar(255) DEFAULT '' COMMENT 'counterparty',
+  `amount` decimal(10,8) DEFAULT '0.00000000' COMMENT 'amount',
+  `currency` varchar(6) DEFAULT '' COMMENT 'currency',
+  `extra` json DEFAULT NULL COMMENT 'extra info about the transaction',
+  `category` varchar(128) NOT NULL DEFAULT '' COMMENT 'category name',
+  `remark` varchar(255) NOT NULL DEFAULT '' COMMENT 'remark',
+  `ctime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'created at',
+  `created_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'created by',
+  `utime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'updated at',
+  `updated_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'updated by',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'record deleted',
+  PRIMARY KEY (`id`),
+  KEY `user_cate_trans_time_idx` (`user_no`,`category`,`trans_time`),
+  KEY `user_trans_time_idx` (`user_no`,`trans_time`),
+  KEY `user_trans_id_idx` (`user_no`,`trans_id`,`deleted`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Cash flow';
