47f08466141a4380654d2fa171cec78d6d1b3ffb
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-19 17:47:27 +0800

    Polishing

--- internal/flow/cashflow.go
+++ internal/flow/cashflow.go
@@ -94,9 +94,6 @@ func ListCashFlows(rail miso.Rail, db *gorm.DB, user common.User, req ListCashFl
 				t.CategoryName = v.Name
 			}
 			t.Amount = money.FormatAmt(t.Amount, t.Currency)
-			if t.Direction == DirectionOut {
-				t.Amount = "-" + t.Amount
-			}
 			return t
 		}).
 		Exec(rail, db)
